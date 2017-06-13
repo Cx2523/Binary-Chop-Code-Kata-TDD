6e18e46b6eea778bda64d1ccaa1286c950045d4d
Christopher Caldwell <clc523@gmail.com>
2017-06-13 14:06:14 -0600

    test cases for function arguments and first parameter now pass

--- spec/spec.js
+++ spec/spec.js
@@ -1,35 +1,45 @@
 describe('Binary search function', function(){
-  describe('Input Data', function(){
-    it('should throw an error if two arguments are not received', function(){
+  describe('should receive two parameters else', function(){
+    it('should throw an error', function(){
       expect(function(){ chop(); }).toThrow();
-      expect(function(){ chop(jasmine.any(Number)); }).toThrow();
+      expect(function(){ chop(1); }).toThrow();
       expect(function(){ chop(1, [], 1); }).toThrow();
     });
-    it('should throw an error if the first argument is not a number', function(){
+  });
+
+  describe('should throw an error if the first argument', function(){
+    it('is not a number', function(){
       expect(function(){ chop('a', []); }).toThrow();
       expect(function(){ chop([], []); }).toThrow();
       expect(function(){ chop({}, []); }).toThrow();
       expect(function(){ chop(true, []); }).toThrow();
     });
-    it('should throw an error if the first argument is not an integer', function(){
+    it('is not an integer', function(){
       expect(function(){ chop(1.2, []) }).toThrow()
     });
-    it('should throw an error if the second argument is not an array', function(){
+  });
+
+  describe('should throw an error if the second argument', function(){
+    it('is not an array', function(){
       expect(function(){ chop(1, 1)}).toThrow();
       expect(function(){ chop(1, 'a')}).toThrow();
       expect(function(){ chop(1, {})}).toThrow();
     });
-    it('should throw an error if the second argument is not an array of integers', function(){
+    it('is not an array of numbers', function(){
       expect(function(){ chop(1, [1, 2, 'a'])}).toThrow();
       expect(function(){ chop(1, [{}, 2, 'a'])}).toThrow();
       expect(function(){ chop(1, [[], 2, 'a'])}).toThrow();
       expect(function(){ chop(1, [null, 2, 'a'])}).toThrow();
     });
-    it('should throw an error if the array of integers is not sorted', function(){
+    it('is not an array of integers',function(){
+      expect(function(){ chop(1, [1, 2, 2.5])}).toThrow();
+    });
+    it('is not sorted', function(){
       expect(function(){ chop(1, [1, 3, 2])}).toThrow();
       expect(function(){ chop(1, [4, 3, 2, 1])}).toThrow();
     });
   });
+
 });
 
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-function chop(searchVal) {
+function chop(searchVal, array) {
+  if( arguments.length !== 2 ){
+    throw 'invalid number of arguments';
+  }
+  if(typeof arguments[0] !== 'number' ) {
+    throw 'first argument must be a number';
+  }
+  if(arguments[0] % 1 !== 0){
+    throw 'first argument must be an integer'
+  }
   return searchVal;
 }
