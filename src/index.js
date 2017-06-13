f7ced6e59f5734cd28223a11601dbf0fc2fdde1d
Christopher Caldwell <clc523@gmail.com>
2017-06-13 14:28:57 -0600

    all tests in input data suite are GREEN

--- src/index.js
+++ src/index.js
@@ -6,7 +6,29 @@ function chop(searchVal, array) {
     throw 'first argument must be a number';
   }
   if(arguments[0] % 1 !== 0){
-    throw 'first argument must be an integer'
+    throw 'first argument must be an integer';
+  }
+  if (!Array.isArray(array)){
+    throw 'second argument must be an array';
+  }
+  if(!arguments[1].every(function(val){
+    return typeof val === 'number';
+  })){
+    throw 'all array values must be numbers';
+  };
+  if(!arguments[1].every(function(val){
+    return val % 1 === 0;
+  })){
+    throw 'all array values must be integers';
+  };
+  var isSorted = true;
+  for (var i = 0; i < array.length - 1; i++){
+    if(array[i + 1] - array[i] < 0) {
+      isSorted = false;
+    }
+  }
+  if(!isSorted){
+    throw 'the array must be sorted';
   }
   return searchVal;
 }
